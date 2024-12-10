71ce466449495459ff24577122aefaafdd88c3a6
olli <56364235+egabosh@users.noreply.github.com>
2024-12-10 11:05:26 +0100

    github specific fixes - thanks to Nils

--- debian/docker/build-debian-env.sh
+++ debian/docker/build-debian-env.sh
@@ -3,12 +3,11 @@
 # install ansible
 apt-get update
 apt-get -y install ansible curl
-#pip install ansible
 
 cd /root
 rm -rf $(hostname -s)-git
 mkdir $(hostname -s)-git
 cd $(hostname -s)-git
 
-curl https://github.com/egabosh/linux-setups/raw/refs/heads/main/debian/basics/basics.yml >basics.yml
+curl -L https://github.com/egabosh/linux-setups/raw/refs/heads/main/debian/basics/basics.yml >basics.yml
 ansible-playbook --connection=local --inventory $(hostname), --limit $(hostname) basics.yml
