eab858ea5f75094a2aa205ed495db87c756b6ceb
olli <56364235+egabosh@users.noreply.github.com>
2025-12-03 09:19:59 +0100

    fix python path

--- debian/docker/build-debian-env.sh
+++ debian/docker/build-debian-env.sh
@@ -10,4 +10,4 @@ mkdir $(hostname -s)-git
 cd $(hostname -s)-git
 
 curl -L https://github.com/egabosh/linux-setups/raw/refs/heads/main/debian/basics/basics.yml >basics.yml
-ansible-playbook --connection=local --inventory $(hostname), --limit $(hostname) -e "ansible_python_interpreter=/usr/bin/python" basics.yml
+ansible-playbook --connection=local --inventory $(hostname), --limit $(hostname) -e "ansible_python_interpreter=/usr/bin/python3" basics.yml
